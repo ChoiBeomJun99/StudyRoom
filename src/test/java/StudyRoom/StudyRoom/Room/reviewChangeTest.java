7d61e73f8eee239d23b5680017fc3d998306e229
Bini <kuninyb@naver.com>
2023-05-12 13:40:27 +0900

    Test : 수정 변경 시간 테스트

--- src/test/java/StudyRoom/StudyRoom/Room/reviewChangeTest.java
+++ src/test/java/StudyRoom/StudyRoom/Room/reviewChangeTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
@@ -43,6 +45,40 @@ public class reviewChangeTest {
 
 
     }
+
+    @Test
+    void 리뷰_변경후_수정시간_변경성공(){
+        // given
+        roomDto roomDto = new roomDto(roomRepository);
+        String roomName = "test123";
+        roomDto.create_room(roomName,10L,15000L,"리뷰 변경 전");
+
+        List test1 = roomRepository.findByroomName(roomName);
+        room room1 = (StudyRoom.StudyRoom.entity.room)test1.get(0);
+
+        LocalDateTime origin_date = room1.getModifiedDate();
+
+        // when
+        changeReview changeReview = new changeReview(roomRepository);
+
+        String newReview = "리뷰 변경 후";
+        changeReview.changeReview(roomName,newReview);
+
+        List test = roomRepository.findByroomName(roomName);
+
+        room room = (StudyRoom.StudyRoom.entity.room) test.get(0);
+
+        LocalDateTime modify_date = room.getModifiedDate();
+
+        // then
+
+        Assertions.assertThat(test.isEmpty()==false);
+        Assertions.assertThat(room.getRoomInformation()).isEqualTo("리뷰 변경 후");
+
+        Assertions.assertThat(origin_date).isNotEqualTo(modify_date);
+
+    }
+
 
 
 }
